1968151ae317294b2e26287a684da7e5bfd283aa
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-03-23 17:14:08 -0500

    infiniteScroll

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -7,17 +7,29 @@ import { Post } from '../../interfaces/interfaces';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
 
   posts: Post[] = [];
 
-  constructor(private postsService: PostsService) {}
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
+    this.siguientesPosts();
+  }
+
+
+  siguientesPosts(event?) {
     this.postsService.getPosts().subscribe(res => {
       console.log(res);
-      this.posts.push(...res.posts)
+      this.posts.push(...res.posts);
+      if (event) {
+        event.target.complete();
+        if (res.posts.length === 0) {
+          event.target.disabled = true;
+        }
+      }
     });
+
   }
 
 }
