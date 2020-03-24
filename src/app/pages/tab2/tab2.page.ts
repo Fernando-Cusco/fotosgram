a8108426944e43055bd01b7441df37e533f49de2
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-03-24 17:47:15 -0500

    obtener las coordenadas del usuario para el post

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PostsService } from '../../service/posts.service';
 import { Router } from '@angular/router';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -9,14 +10,14 @@ import { Router } from '@angular/router';
 })
 export class Tab2Page {
   tempImages: string[] = [];
-
+  cargandoUbicacion: boolean = false;
   post = {
     mensaje: '',
     coords: null,
     posicion: false
   }
 
-  constructor(private postService: PostsService, private route: Router) {}
+  constructor(private postService: PostsService, private route: Router, private geolocation: Geolocation) {}
 
 
   async crearPost() {
@@ -27,6 +28,23 @@ export class Tab2Page {
       posicion: false
     };
     this.route.navigateByUrl('/main/tabs/tab1');
+  }
+
+  getUbicacion() {
+    if(!this.post.posicion) {
+      this.post.coords = null;
+      this.cargandoUbicacion = false;
+      return;
+    }
+    this.cargandoUbicacion = true;
+    this.geolocation.getCurrentPosition().then((resp) => {
+      const coords = `${resp.coords.latitude},${resp.coords.longitude}`;
+      this.post.coords = coords;
+      this.cargandoUbicacion = false;
+     }).catch((error) => {
+      this.cargandoUbicacion = false;
+     });
+    
   }
 
 }
