319454acfda0ac4e2cbebf92b8e02dc4a1a0479c
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-03-24 20:26:54 -0500

    agregando el mapa, mapbox

---
+++ src/app/components/mapa/mapa.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+
+declare var mapboxgl: any;
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.scss'],
+})
+export class MapaComponent implements OnInit {
+  @Input() coords: string;
+  @ViewChild('mapa', {static: true}) mapa;
+  constructor() { }
+
+  ngOnInit() {
+    const latLng = this.coords.split(',');
+    const lat = Number(latLng[0]);
+    const lng = Number(latLng[1]);
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZG9jdXNjbyIsImEiOiJjazg2aTFhNHcwNmR5M21samtsenZib3FxIn0.uDphCYwU4J6zK4_AyXXtmA';
+    const map = new mapboxgl.Map({
+      container: this.mapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: 15
+    });
+
+    const marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
+
+  }
+}
